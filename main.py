7e51edb210e504d0c8420464a7b7edf942f03b5e
ColinWWW <colin.wallace001@gmail.com>
2025-01-28 19:27:31 -0500

    gives letter count etc

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ def main():
         word_count = f"{word_number}"
         file_contents_lower = file_contents.lower().split()
         alphabet = list("abcdefghijklmnopqrstuvwxyz")
-        # print(alphabet)
+
         for words in file_contents_lower:
             for letters in words:
                 if letters in alphabet and letters in letter_tracker:
                     letter_tracker[letters] += 1
                 elif letters in alphabet:
                     letter_tracker[letters] = 1
+        #Start of statement
+        print(f"-- Begin report of books/frankenstein.txt --- ")
+        #number of words
+        print(f"{word_count} words found in the document")
 
-        print(letter_tracker)
-        print(word_count)
-
+        for values in sorted(letter_tracker):
+            print(f"The '{values}' character was found {letter_tracker[values]} times")
+        print("--- End report ---")
 
 main()
